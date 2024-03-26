09a74a88d4d554f71c02fbe2c47ff45e312d831e
gcs-abdulwahab <100867378+gcs-abdulwahab@users.noreply.github.com>
2024-03-26 11:31:24 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\PatientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::apiResource('employees', EmployeeController::class);
+
+Route::apiResource('patients' , PatientController::class);

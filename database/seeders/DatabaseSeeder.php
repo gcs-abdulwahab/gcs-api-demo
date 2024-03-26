d5c8a4a92b6966174cc195a57193308810813bec
gcs-abdulwahab <100867378+gcs-abdulwahab@users.noreply.github.com>
2024-03-26 11:19:36 +0500

    Patient Crud Created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Patient;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        User::create([
+            'name' => 'Abdul Wahab',
+            'email' => 'abdulwahab@abdulwahab.com',
+            'password' => bcrypt('abdulwahab@abdulwahab.com'),
+        ]);
+
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Patient::factory(10)->create();
     }
 }
